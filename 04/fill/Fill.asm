dcd9f63ff477d2f7532e851bafbe782673eada89
Sebastian- <hmurgu@interchange.ubc.ca>
2019-05-20 09:27:23 -0700

    implemented fill assembly program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,95 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+@isScreenFilled
+M=0
+
+(INFLOOP)
+  // reset screen address offset
+  @i
+  M=0
+
+  // check if key is pressed
+  @KBD
+  D=M
+  @KEYPRESSED
+  D;JGT
+  @KEYNOTPRESSED
+  0;JMP
+
+  // fill screen if it's not already filled
+  (KEYPRESSED)
+    @isScreenFilled
+    D=M
+    @INFLOOP
+    D;JGT
+    @FILLSCREEN
+    0;JMP
+  
+  // clear screen if it's not already cleared
+  (KEYNOTPRESSED)
+    @isScreenFilled
+    D=M
+    @CLEARSCREEN
+    D;JGT
+    @INFLOOP
+    0;JMP
+
+  (CLEARSCREEN)
+    // bounds check for screen memory - total of 32 * 256 registers
+    @8192
+    D=A
+    @i
+    D=D-M
+    @CLEARSCREENEND
+    D;JLE
+
+    // clear register
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=0
+
+    // i++
+    @i
+    M=M+1
+    @CLEARSCREEN
+    0;JMP
+
+  (CLEARSCREENEND)
+    @isScreenFilled
+    M=0
+    @INFLOOP
+    0;JMP
+  
+  (FILLSCREEN)
+    // bounds check for screen memory - total of 32 * 256 registers
+    @8192
+    D=A
+    @i
+    D=D-M
+    @FILLSCREENEND
+    D;JLE
+
+    // fill register
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    A=D
+    M=-1
+
+    // i++
+    @i
+    M=M+1
+    @FILLSCREEN
+    0;JMP
+
+  (FILLSCREENEND)
+    @isScreenFilled
+    M=1
+    @INFLOOP
+    0;JMP
