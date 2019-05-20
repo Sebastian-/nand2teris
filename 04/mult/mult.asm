6fa8f5dff3df4de58e642090296b0476cdfd4a4d
Sebastian- <hmurgu@interchange.ubc.ca>
2019-05-19 21:35:26 -0700

    implemented multiplication in machine language

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -5,5 +5,35 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Does so by adding the value in RAM[0] to itself RAM[1] times
 
-// Put your code here.
+// initialize
+@count
+M=0
+@R2
+M=0
+
+(LOOP)
+  // check if count < RAM[1]
+  @count
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JGE
+
+  // RAM[2] = RAM[2] + RAM[0]
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  // count++
+  @count
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
