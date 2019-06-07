8f8994d00eb9ce05aa90278b247eec82ce32248d
Sebastian- <hmurgu@interchange.ubc.ca>
2019-06-07 09:08:35 -0700

    Refactored HackAssembler into a class

--- 06/assembler/HackAssembler.py
+++ 06/assembler/HackAssembler.py
@@ -5,69 +5,83 @@ from CInstrTranslator import CInstrTranslator
 from SymbolTable import SymbolTable
 
 
-def initSymbolTable():
-  '''Initializes a symbol table with predefined values'''
-  table = SymbolTable()
-  table.addEntry('SP', 0)
-  table.addEntry('LCL', 1)
-  table.addEntry('ARG', 2)
-  table.addEntry('THIS', 3)
-  table.addEntry('THAT', 4)
-  table.addEntry('SCREEN', 16384)
-  table.addEntry('KBD', 24576)
-  for i in range(16):
-    table.addEntry('R' + str(i), i)
-  
-  return table
+class HackAssember:
+  def __init__(self):
+    self.symbolTable = SymbolTable()
+    self.symbolTable.addEntry('SP', 0)
+    self.symbolTable.addEntry('LCL', 1)
+    self.symbolTable.addEntry('ARG', 2)
+    self.symbolTable.addEntry('THIS', 3)
+    self.symbolTable.addEntry('THAT', 4)
+    self.symbolTable.addEntry('SCREEN', 16384)
+    self.symbolTable.addEntry('KBD', 24576)
+    for i in range(16):
+      self.symbolTable.addEntry('R' + str(i), i)
 
 
-def firstPass(file):
-  '''Populates symbol table with label addresses'''
-  parser = Parser(file)
-  table = initSymbolTable()
-  instructionAddress = 0
+  def firstPass(self, asmFile):
+    '''Populates symbol table with label addresses'''
+    parser = Parser(asmFile)
+    instructionAddress = 0
+    
+    while parser.hasMoreCommands():
+      parser.advance()
+      if parser.commandType() == 'L_COMMAND':
+        self.symbolTable.addEntry(parser.symbol(), instructionAddress)
+      else:
+        instructionAddress += 1
   
-  while parser.hasMoreCommands():
-    parser.advance()
-    if parser.commandType() == 'L_COMMAND':
-      table.addEntry(parser.symbol(), instructionAddress)
+
+  def assembleACommand(self, symbol, freeMemoryAddress):
+    '''Assembles A commands (eg. @1234, @R0, or @index) into hack binary
+       Returns the binary command and a boolean indicating whether a new
+       symbol was added to the symbol table'''
+    aCommandFormat = '0>16b'
+
+    if symbol[0].isnumeric():
+      # eg. @1234
+      return format(int(symbol), aCommandFormat), False
+    elif self.symbolTable.contains(symbol):
+      # eg. @R0
+      return format(int(self.symbolTable.getAddress(symbol)), aCommandFormat), False
     else:
-      instructionAddress += 1
-  
-  return table
+      # add new symbol to table
+      self.symbolTable.addEntry(symbol, freeMemoryAddress)
+      return format(freeMemoryAddress, aCommandFormat), True
 
+
+  def assembleCCommand(self, comp, dest, jump):
+    binaryCommand = '111'
+    binaryCommand += CInstrTranslator.translateComp(comp)
+    binaryCommand += CInstrTranslator.translateDest(dest)
+    binaryCommand += CInstrTranslator.translateJump(jump)
+    return binaryCommand
+
+
+
+  def assemble(self, asmFile):
+    self.firstPass(asmFile)
+    hackFilename = asmFile[:-4] + '.hack'
+    parser = Parser(asmFile)
+    freeMemoryAddress = 16
+
+    with open(hackFilename, 'w') as hackBinary:
+      while parser.hasMoreCommands():
+        parser.advance()
+
+        if parser.commandType() == 'A_COMMAND':
+          binaryACommand, allocatedMemory = self.assembleACommand(parser.symbol(), freeMemoryAddress)
+          hackBinary.write(binaryACommand + '\n')
+          if allocatedMemory:
+            freeMemoryAddress += 1
+          
+        if parser.commandType() == 'C_COMMAND':
+          binaryCCommand = self.assembleCCommand(parser.comp(), parser.dest(), parser.jump())
+          hackBinary.write(binaryCCommand + '\n')
 
 
 if __name__ == "__main__":
-  asmFilename = sys.argv[1]
-  hackFilename = asmFilename[:-4] + '.hack'
-  symbolTable = firstPass(asmFilename)
-  parser = Parser(asmFilename)
-  freeMemoryAddress = 16
-
-  with open(hackFilename, 'w') as hackBinary:
-    while parser.hasMoreCommands():
-      parser.advance()
-
-      if parser.commandType() == 'A_COMMAND':
-        aCommandFormat = '0>16b'
-        symbol = parser.symbol()
-        if symbol[0].isnumeric():
-          # @number
-          hackBinary.write(format(int(symbol), aCommandFormat) + '\n')
-        elif symbolTable.contains(symbol):
-          # @label
-          hackBinary.write(format(symbolTable.getAddress(symbol), aCommandFormat) + '\n')
-        else:
-          # @variable
-          symbolTable.addEntry(symbol, freeMemoryAddress)
-          hackBinary.write(format(freeMemoryAddress, aCommandFormat) + '\n')
-          freeMemoryAddress += 1
-
-      if parser.commandType() == 'C_COMMAND':
-        binaryCommand = '111'
-        binaryCommand += CInstrTranslator.translateComp(parser.comp())
-        binaryCommand += CInstrTranslator.translateDest(parser.dest())
-        binaryCommand += CInstrTranslator.translateJump(parser.jump())
-        hackBinary.write(binaryCommand + '\n')
+  asmFile = sys.argv[1]
+  assembler = HackAssember()
+  assembler.assemble(asmFile)
 
